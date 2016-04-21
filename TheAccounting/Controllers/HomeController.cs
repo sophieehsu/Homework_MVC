228efdfd4aa6da3866a44d1344ca23cded893973
sophiee <sophiee_hsu@outlook.com>
2016-04-21 20:41:06 +0800

    套用樣版(套上程式)

--- TheAccounting/Controllers/HomeController.cs
+++ TheAccounting/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheAccounting.Models.ViewModels;
 
 namespace TheAccounting.Controllers
 {
@@ -11,6 +12,17 @@ namespace TheAccounting.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult List()
+        {
+            var model = new List<FeeData> {
+              new FeeData {Id=1, FeeType=enFeeType.Pay, FeeDay=new DateTime(2016,1,1), Amount=300 },
+              new FeeData {Id=2, FeeType=enFeeType.Pay, FeeDay=new DateTime(2016,1,2), Amount=1600 },
+              new FeeData {Id=3, FeeType=enFeeType.Pay, FeeDay=new DateTime(2016,1,3), Amount=800 }
+            };
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ TheAccounting/Models/ViewModels/FeeData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheAccounting.Models.ViewModels
+{
+    public class FeeData
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 類別
+        /// </summary>
+        public enFeeType FeeType { get; set; }
+
+        /// <summary>
+        /// 金額
+        /// </summary>
+        public double Amount { get; set; }
+
+        /// <summary>
+        /// 日期
+        /// </summary>
+        public DateTime FeeDay { get; set; }
+
+        /// <summary>
+        /// 備註
+        /// </summary>
+        public string Memo { get; set; }
+    }
+
+    public enum enFeeType
+    {
+        /// <summary>
+        /// 收入
+        /// </summary>
+        Income = 1,
+
+        /// <summary>
+        /// 支出
+        /// </summary>
+        Pay = 2
+    }
+}
