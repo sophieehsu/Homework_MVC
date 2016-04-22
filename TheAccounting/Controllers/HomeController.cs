84a254c23c9f39c79b816633ba080de1cc41ffd5
sophiee <sophiee_hsu@outlook.com>
2016-04-22 10:18:55 +0800

    改存取DB資料

--- TheAccounting/Controllers/HomeController.cs
+++ TheAccounting/Controllers/HomeController.cs
@@ -1,32 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using TheAccounting.Models;
 using TheAccounting.Models.ViewModels;
 
 namespace TheAccounting.Controllers
 {
     public class HomeController : Controller
     {
-        public static List<FeeData> myFeeData { get; set; }
-
+        FeeModels db = new FeeModels();
+        
         public ActionResult Index()
         {
-            myFeeData = new List<FeeData> {
-              new FeeData {Id=1, FeeType=enFeeType.支出, FeeDay=new DateTime(2016,1,1), Amount=300 },
-              new FeeData {Id=2, FeeType=enFeeType.支出, FeeDay=new DateTime(2016,1,2), Amount=1600 },
-              new FeeData {Id=3, FeeType=enFeeType.支出, FeeDay=new DateTime(2016,1,3), Amount=800 }
-            };
-
             return View();
         }
 
         [HttpPost]
         public ActionResult Index(FeeData model)
         {
-            model.Id = myFeeData.Max(x => x.Id) + 1;
-            myFeeData.Add(model);
+            var data = new AccountBook()
+            {
+                Id = Guid.NewGuid(),
+                Categoryyy = (int)model.FeeType,
+                Amounttt = (int)model.Amount,
+                Dateee = model.FeeDay,
+                Remarkkk = model.Memo
+            };
+
+            db.AccountBook.Add(data);
+            db.SaveChanges();
 
             return View();
         }
@@ -34,6 +37,15 @@ namespace TheAccounting.Controllers
         [ChildActionOnly]
         public ActionResult List()
         {
+            // 取出最新的10筆紀錄
+            var myFeeData = from d in db.AccountBook.OrderByDescending(x => x.Dateee).Take(10).OrderBy(x => x.Dateee)
+                            select new FeeData
+                            {
+                                FeeType = (enFeeType)d.Categoryyy,
+                                Amount = d.Amounttt,
+                                FeeDay = d.Dateee
+                            };
+
             return View(myFeeData);
         }
 
